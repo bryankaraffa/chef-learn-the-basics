16dd1b184ffccf4ded80cdceeb1d731a73cb5f13
Bryan Karaffa <bryankaraffa@gmail.com>
2015-03-04 15:37:41 -0800

    Moved recipes into recipes folder 	recipes/

--- hello.rb
+++
@@ -1,8 +0,0 @@
-# This is a simple example of a recipe which creates a file `motd`
-
-directory '/tmp/messages'
-
-file '/tmp/messages/motd' do
-   action :create 		# :create is not needed because it is the default action for a `file` resource
-   content 'hello chef'
-end
