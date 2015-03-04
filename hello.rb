3af390c253a9e8efe1b3106fffb3d8ed32de0b40
Bryan Karaffa <bryankaraffa@gmail.com>
2015-03-04 15:17:23 -0800

    Added comments to hello.rb to better describe what is going on

--- hello.rb
+++ hello.rb
@@ -1,3 +1,5 @@
+# This is a simple example of a recipe which creates a file `motd`
 file 'motd' do
+   action :create 		# :create is not needed because it is the default action for a `file` resource
    content 'hello chef'
 end
