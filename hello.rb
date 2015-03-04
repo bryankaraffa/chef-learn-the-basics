3a5606cc0677b043049ecd74f594c33784dbe1c2
Bryan Karaffa <bryankaraffa@gmail.com>
2015-03-04 15:28:33 -0800

    modified:   hello.rb Modify the hello.rb recipe you wrote in this lesson to manage the MOTD file under the /tmp/messages directory, and not in the current directory.

--- hello.rb
+++ hello.rb
@@ -1,5 +1,8 @@
 # This is a simple example of a recipe which creates a file `motd`
-file 'motd' do
+
+directory '/tmp/messages'
+
+file '/tmp/messages/motd' do
    action :create 		# :create is not needed because it is the default action for a `file` resource
    content 'hello chef'
 end
